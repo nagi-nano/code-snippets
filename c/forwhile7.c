2b0e9665197caecb7c209a9fc98c7a9f8e5cb57f
nagi <66349084+nagi-nano@users.noreply.github.com>
2021-01-21 21:38:33 +0900

    Update forwhile7.c

--- c/forwhile7.c
+++ c/forwhile7.c
@@ -14,7 +14,7 @@ int main()
         {
                 for(i = 2; i <= n; i++)
                 {
-                    if(n % i == 0)
+                    while(n % i == 0)
                     {
                         printf("%d", i);
                         n /= i;
