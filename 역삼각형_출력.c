676d873d80b4937a1dbfa8ffee08890e12d2e8c9
nagi <66349084+nagi-nano@users.noreply.github.com>
2021-01-21 17:19:58 +0900

    Create 역삼각형_출력.c

---
+++ 역삼각형_출력.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i,j,n;
+    printf("임의의 수를 입력해 주세요 : ");
+    scanf("%d", &n);
+
+    for(i = n; i >= 1; i--)
+    {
+        for(j = 0; j < i*2-1; j++)
+                printf("*");
+        printf(" ");
+    }
+    printf("\n");
+    return 0;
+}
